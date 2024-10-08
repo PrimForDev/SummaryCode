54554b5a1d1770075a72329400fbfb874d9559e5
titiporn <titiporn.sinpaksa@intellity.co.th>
2024-10-08 11:09:38 +0700

    update001

--- Javascript/Demo001/script.js
+++ Javascript/Demo001/script.js
@@ -23,8 +23,90 @@ document.getElementById('title').innerText = 'สวัสดีครับ Jav
 // console.log(typeof sayHello)
 
 //number type
-let n1 = 12 //Int
-let n2 = 12.52 //Floating
-let n3 = NaN // Not a Number เกิดจากการแปลงค่าแต่ค่านั้นไม่ใช่ number
-let n4 = Infinity
-console.log(n1, n2, n3, n4)
+// let n1 = 12 //Int
+// let n2 = 12.52 //Floating
+// let n3 = NaN // Not a Number เกิดจากการแปลงค่าแต่ค่านั้นไม่ใช่ number
+// let n4 = Infinity
+// console.log(n1, n2, n3, n4)
+
+// let a = 4
+// let b = 3
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b) //หารเอาเศษ
+// console.log(a ** b) //ยกกำลัง2
+
+// console.log(a++)
+// console.log(a--)
+// console.log(++b)
+// console.log(--b)
+
+// let count = 1
+// count += 10
+// console.log(count)
+
+//workshop
+// let days = 1
+// console.log(days*24)
+
+//string
+// let s1 = 'Hello'
+// let s2 = "Hello"
+// console.log(s1, s2)
+
+// let firstName = 'John'
+// let lastName = 'Doe'
+// let age = 18
+// console.log(firstName + ' ' + lastName + ' (' + age + ')')
+
+// //teplate literals (backlist)
+// console.log(`${firstName} ${lastName} (${age})`)
+
+// let x = 10
+// let y = 5
+// console.log(`${x} + ${y} = ${x + y}`)
+
+// //escape character
+// console.log("I'm \"programmer\"")
+// console.log('I\'m "programmer"')
+// console.log(`I'm "programmer"`)
+// console.log('1\n\n2\n\n3')
+
+//workshop
+// let name1 = 'John'
+// let name2 = 'Joe'
+// let name3 = 'Jane'
+
+// // 1. John
+// // 2. Joe
+// // 3. Jane
+// console.log(`1. ${name1}
+// 2. ${name2}
+// 3. ${name3}\n`)
+
+// console.log(`1. ${name1}\n2. ${name2}\n3. ${name3}\n`)
+
+//Interaction
+// let firstName = 'John'
+// let lastName = 'Doe'
+// alert(`ชื่อของคุณคือ ${firstName} ${lastName}`)
+// let firstName = prompt('ระบุชื่อจริงของคุณ')'ระบุชื่อจริงของคุณ')
+// let lastName = prompt('ระบุนามสกุลของคุณ')
+// alert(`ชื่อของคุณคือ ${firstName} ${lastName}`)
+
+// let x = prompt('x')
+// let y = prompt('y')
+// alert(Number(x)+Number(y))
+ 
+// let areyousure = confirm('Are you sure?')
+// alert(areyousure)
+
+//workshop
+// let x = prompt('X:')
+// let y = prompt('Y:')
+// alert(`${x}+${y}=${Number(x)+Number(y)}
+// ${x}-${y}=${Number(x)-Number(y)}
+// ${x}*${y}=${Number(x)*Number(y)}
+// ${x}/${y}=${Number(x)/Number(y)}`)
