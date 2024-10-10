54de01f077a9314e9a1846c048b47501c287c1e6
titiporn <titiporn.sinpaksa@intellity.co.th>
2024-10-10 10:22:29 +0700

    update1

--- Javascript/Demo001/script.js
+++ Javascript/Demo001/script.js
@@ -522,19 +522,70 @@ document.getElementById('title').innerText = 'สวัสดีครับ Jav
 //     result += n
 // }
 // console.log(result)
-let set = [2, 1, 3, 5, 4, 10, 8, 2, -1, 5]
-let max = set[0]
-let min = set[0]
-for(let n of set){
-    if(n < min){ 
-        min = n
-    }
-    if(n > max){
-        max = n
-    }
-}
+// let set = [2, 1, 3, 5, 4, 10, 8, 2, -1, 5]
+// let max = set[0]
+// let min = set[0]
+// for(let n of set){
+//     if(n < min){ 
+//         min = n
+//     }
+//     if(n > max){
+//         max = n
+//     }
+// }
 
-console.log(`min: ${min}`)
-console.log(`max: ${max}`)
+// console.log(`min: ${min}`)
+// console.log(`max: ${max}`)
+
+//#############1.20 Loop Nested ลูปซ้อนลูป####################
+// let game = [
+//     ['O','O','X'],
+//     ['X','X','O'],
+//     ['O','X','O']
+// ]
+
+// console.log(game)
+// console.log(`${game[0][0]} ${game[0][1]} ${game[0][2]}`)
+// console.log(`${game[1][0]} ${game[1][1]} ${game[1][2]}`)
+// console.log(`${game[2][0]} ${game[2][1]} ${game[2][2]}`)
+
+// let outputString = ''
+
+// for(let row of game){
+//     // console.log(row)
+//     let rowString = ''
+//     for(let field of row){
+//         // console.log(field)
+//         rowString += field + ' '
+//     }
+//     console.log(rowString)
+//     outputString += rowString + '\n'
+// }
+
+// let rowCount = 5
+// let outputString = ''
+
+// for(let row = 1; row <= rowCount; row++){
+//     let rowString = ''
+//     for(let star = 1; star <= row; star++){
+//         rowString += '* '
+//     }
+//     outputString += rowString + '\n'
+// }
+// console.log(outputString)
 
 
+// let rowCount = 5
+// let outputString = ''
+
+// for(let row = 1; row <= rowCount; row++){
+//     let rowString = ''
+//     for(let dash = rowCount; dash >= row; dash--){
+//         rowString += '- '
+//     }
+//     for(let star = 1; star <= row; star++){
+//         rowString += '* '
+//     }
+//     outputString += rowString + '\n'
+// }
+// console.log(outputString)
